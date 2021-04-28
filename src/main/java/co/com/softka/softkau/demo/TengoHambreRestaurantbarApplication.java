ef971a8b705f7cce6b285d7ac0fc7b7881cc7cae
David uribe <Daviduribe0007@gmail.com>
2021-04-28 14:46:43 -0500

    Se crea el setup de la aplicacion

---
+++ src/main/java/co/com/softka/softkau/demo/TengoHambreRestaurantbarApplication.java
@@ -0,0 +1,13 @@
+package co.com.softka.softkau.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TengoHambreRestaurantbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TengoHambreRestaurantbarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/softka/softkau/demo/TengoHambreRestaurantbarApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.softka.softkau.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TengoHambreRestaurantbarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
