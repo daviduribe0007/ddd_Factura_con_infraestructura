ff1e70bb985beaf549f69704cd4eeef9dcd1d750
David uribe <Daviduribe0007@gmail.com>
2021-04-28 18:13:31 -0500

    Se crea el eventchange para factura llamado FacturaChange

--- src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/Factura.java
+++ src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/Factura.java
@@ -7,19 +7,26 @@ import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.*;
 import java.util.Map;
 
 public class Factura extends AggregateEvent<FacturaId> {
+
+    protected Camarero camarero;
+    protected Consumidor consumidor;
+    protected Fecha fecha;
+    protected Iva iva;
+    protected Map<ProductoId,Producto> productos;
     protected Dinero propina;
     protected Dinero subtotal;
     protected Dinero total;
-    protected Iva iva;
-    protected Consumidor consumidor;
-    protected Camarero camarero;
-    protected Map<ProductoId,Producto> productos;
-    protected Fecha fecha;
+
 
 
     public Factura(FacturaId facturaId,Fecha fecha) {
         super(facturaId);
         appendChange(new FacturaCreada(facturaId,fecha)).apply();
+    }
+
+    private Factura(FacturaId facturaId){
+        super(facturaId);
+        subscribe(new FacturaChange(this));
     }
 
 
---
+++ src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/FacturaChange.java
@@ -0,0 +1,23 @@
+package co.com.softka.softkau.tengohambrerestaurantbar.domain.factura;
+
+import co.com.sofka.domain.generic.EventChange;
+import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.events.FacturaCreada;
+import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.Dinero;
+import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.Iva;
+
+import java.util.HashMap;
+
+public class FacturaChange extends EventChange {
+
+    public FacturaChange(Factura factura) {
+
+        apply((FacturaCreada event) -> {
+            factura.fecha = event.getFecha();
+            factura.productos = new HashMap<>();
+            factura.subtotal = new Dinero(0);
+            factura.iva = new Iva(0.1f);
+            factura.propina = new Dinero(0)
+            factura.total = new Dinero(0);
+        });
+    }
+}
