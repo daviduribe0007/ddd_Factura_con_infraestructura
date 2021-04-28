6d220ee953a01ea33d574c7ddbdb693b44306170
David uribe <Daviduribe0007@gmail.com>
2021-04-28 17:31:39 -0500

    Se crea el value object Fecha

--- src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/Factura.java
+++ src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/Factura.java
@@ -4,15 +4,23 @@ import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.FacturaId;
 import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.Dinero;
 import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.Iva;
+import co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values.ProductoId;
+
+import java.util.Map;
 
 public class Factura extends AggregateEvent<FacturaId> {
     protected Dinero propina;
     protected Dinero subtotal;
     protected Dinero total;
     protected Iva iva;
+    protected Consumidor consumidor;
+    protected Camarero camarero;
+    protected Map<ProductoId,Producto> productos;
 
 
-    public Factura(FacturaId entityId) {
+    public Factura(FacturaId entityId,) {
         super(entityId);
     }
+    private Factura(FacturaId )
+
 }
---
+++ src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/values/Fecha.java
@@ -0,0 +1,19 @@
+package co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Fecha implements ValueObject<String> {
+
+    private final String value;
+
+    public Fecha(String nombre) {
+        this.value = Objects.requireNonNull(nombre);
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
