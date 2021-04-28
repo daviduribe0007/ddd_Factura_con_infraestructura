bcda3254b7f7a0994b8f0bf6764d8aeccce85e31
David uribe <Daviduribe0007@gmail.com>
2021-04-28 15:59:07 -0500

    Se crea el value object descripcion

---
+++ src/main/java/co/com/softka/softkau/tengohambrerestaurantbar/domain/factura/values/Descripcion.java
@@ -0,0 +1,19 @@
+package co.com.softka.softkau.tengohambrerestaurantbar.domain.factura.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Descripcion implements ValueObject<String> {
+
+    private final String value;
+
+    public Descripcion(String descripcion) {
+        this.value = Objects.requireNonNull(descripcion);
+    }
+
+    @Override
+    public String value() {
+        return value;
+    }
+}
